e8e445c5b2f7b0efac378d2be7ec432f1c70eb1c
Eightaan <57832730+Eightaan@users.noreply.github.com>
2024-10-21 17:07:25 +0200

    Delete lua/MainMenu.lua

--- lua/MainMenu.lua
+++
@@ -1,5 +0,0 @@
-Hooks:PostHook(MenuSceneManager, "_set_up_environments", "HMH_MenuSceneManager_set_up_environments", function(self)
-	if EIVHUD.Options:GetValue("MENU/MenuFilter") and self._environments and self._environments.standard and self._environments.standard.color_grading then
-		self._environments.standard.color_grading = "color_off"
-	end
-end)
