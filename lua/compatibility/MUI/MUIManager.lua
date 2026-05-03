7f6cfc6de49232c79908c347f028017f1b47899a
Eightaan <57832730+Eightaan@users.noreply.github.com>
2026-05-03 22:46:08 +0200

    Add files via upload

--- lua/compatibility/MUI/MUIManager.lua
+++ lua/compatibility/MUI/MUIManager.lua
@@ -2,4 +2,14 @@ if ArmStatic and MUIMenu and MUIMenu:ClassEnabled("MUIStats") and MUIStats then
     Hooks:Add("HUDManagerSetupPlayerInfoHudPD2", "EIVHUD_MUI_setup", function(self)
 		dofile(EIVHUD.ModPath .. "lua/compatibility/MUI/MUIStats.lua")
     end)
+
+	function HUDConverts:init(hud) end
+	
+	function HUDConverts:update() end
+	
+	function HUDInspire:update_position()
+		local offset = 5
+		self._inspire_panel:set_top(0)
+		self._inspire_panel:set_right(self._hud_panel:w() + offset)
+	end
 end
